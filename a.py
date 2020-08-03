8fcb990383911527fb4c597fe8c2389c1e637639
Paul Soulanille <paul@soulanille.net>
2020-08-02 21:01:26 -0400

    removed computer specific file references

--- a.py
+++ a.py
@@ -31,12 +31,10 @@ flt = IIR()
 import fmsynth as fm
 import signals as sig
 from itools import *
-f1 = aud.audioFile("/Users/paul/Music/other/chipzel\ -\ Spectra\ -\ 10\ Sonnet-FR7MMf4A5Ic.mkv".replace("\\",""))
-f2 = aud.audioFile("/Users/paul/Music/other/Virtual\ Riot\ -\ Idols\ \(EDM\ Mashup\)\ \(Official\ Video\)-vZyenjZseXA.mkv".replace("\\",""))
-f3 = aud.audioFile("/Users/paul/Music/other/_-5IOVkstxkdE.mkv".replace("\\",""))
-f4 = aud.audioFile("/Users/paul/Music/other/Bossfight\ -\ Milky\ Ways-_5w8SJ3yVsc.webm".replace("\\",""))
-f5 = aud.audioFile("/Users/paul/Music/other/Mr.\ Blue\ Sky-wuJIqmha2Hk.webm".replace("\\",""))
-f6 = aud.audioFile("/Users/paul/Music/other/Initial\ D\ -\ Night\ Of\ Fire-SRbhLtjOiRc.mp4".replace("\\",""))
+
+from songs import *
+
+
 g1 = None
 g2 = None
 g3 = None
@@ -63,7 +61,6 @@ def bpm(b,sr = 48000):
     return (60/b)*sr
  
 
-primitives = aud.audioFile("/Users/paul/Music/other/osciloscope music/Primitives.wav")
 
 
 import numpy as np
@@ -115,22 +112,3 @@ fld4 = filt.feedbackl(filt.chainl(filt.delayl(22154*4-2000),di.tritapsl(.5,2*200
 fla = filt.feedbackl(filt.chainl(filt.delayl(22154)),di.maxholdfuncl(lambda v,m: v/(m+1),.995),o(lambda x:x))
 flao = filt.feedbackl(filt.chainl(filt.delayl(22154-2000),filt.parrl(filt.delayl(2000),di.tritapsl(.5,2*2000))),di.maxholdfuncl(lambda v,m: v/(m+1),.995),o(lambda x:x))
 
-
-
-
-dinf =  aud.audioFile("/Users/paul/Music/other/chiptune/Deathro\ -\ I\ Never\ Forget.wav".replace("\\",""),165)
-v = aud.audioFile("/Users/paul/Music/other/virtual\ riot/Virtual\ Riot\ -\ Transmission-AJlIYAsSTws.mkv".replace("\\",""),130)
-bh = aud.audioFile("/Users/paul/Music/other/Basshunter\ -\ DotA\ \(HQ\)-8d44ykdKvCw.webm".replace("\\",""))
-h = aud.audioFile("/Users/paul/Music/other/muse/Muse\ -\ Knights\ Of\ Cydonia\ \ \(Video\)-G_sBOsh-vyI.mkv".replace("\\",""))
-cd = aud.audioFile("/Users/paul/Music/other/すてらべえ\ BREAKCORE\ \(Stella\ Hiragana\)\ -\ Caramelldansen\ Remix\ _\ ｳｯｰｳｯｰｳﾏｳﾏ\(ﾟ∀ﾟ\)-87fh7xHZVvc.webm".replace("\\",""))
-fd = aud.audioFile("/Users/paul/Music/other/XI\ -\ Freedom\ Dive\ ↓-OI3C9qQlb1U.webm".replace("\\",""))
-cdbr = aud.audioFile("/Users/paul/Music/other/Weird\ russian\ singer\ -\ Chum\ Drum\ Bedrum-tVj0ZTS4WF4.mkv".replace("\\",""))
-um = aud.audioFile("/Users/paul/Music/other/TheFatRat\ -\ Unity\ vs\ Megalovania\ \(by\ LiterallyNoOne\)-um_yP-2ix9k.webm".replace("\\",""))
-rwby = aud.audioFile("/Users/paul/Music/other/RWBY\ Volume\ 1\ Soundtrack\ -\ 8.\ I\ May\ Fall-GGoiOfHsuUA.webm".replace("\\",""))
-nbp = aud.audioFile("/Users/paul/Music/other/NOMA\ -\ Brain\ Power\ -\ LYRICS\!-h-mUGj41hWA.mkv".replace("\\",""))
-sf = aud.audioFile("/Users/paul/Music/other/Getsix\ -\ Sky\ Fracture-_PDCcQ3v1MY.mkv".replace("\\",""))
-sf2=aud.audioFile("/Users/paul/Music/other/getsix/Getsix\ -\ Sky\ Fracture\ VIP\ \(ft.\ Miss\ Lina\)-4zeZgdQX1SE.mkv".replace("\\",""))
-f74 = aud.audioFile("/Users/paul/Music/other/f-777/F\ 777\ -\ 4.\ I\ Love\ You\ All\ \(Ludicrous\ Speed\ Album\)-EorbESm9LSc.mkv".replace("\\",""))
-f7lia = aud.audioFile("/Users/paul/Music/other/f-777/jesseValentineMusic/F-777\ -\ Life\ Is\ Awesome-5ExTKw3y7q0.webm".replace("\\",""))
-f7lisa = aud.audioFile("/Users/paul/Music/other/f-777/jesseValentineMusic/F-777\ -\ Life\ Is\ Still\ Awesome\ \(COSMIC\ BLASTER\)-K6AAql-rPqI.webm".replace("\\",""))
-    
